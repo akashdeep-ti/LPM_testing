8df04237d764b4e6514f46a1f51d0a40e58eab90
akashdeep-ti <a-kaur@ti.com>
2024-12-04 18:26:17 +0530

    Create deep sleep and mcu only stress test

---
+++ deep_sleep_and_mcu_only_stress_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+i=0
+mcu_only_latency=50
+deep_sleep_latency=150
+while :
+do
+        resume_latency=$((($RANDOM % deep_sleep_latency) + mcu_only_latency))
+        echo $resume_latency > /sys/devices/system/cpu/cpu0/power/pm_qos_resume_latency_us
+        if [ "$resume_latency" -gt "9" ] && [ "$resume_latency" -lt "101" ]
+        then
+          echo "MCU Only mode selected"
+        elif [ "$resume_latency" -gt "100" ] && [ "$resume_latency" -lt "251" ]
+        then
+          echo "Deep Sleep mode selected"
+        else
+          echo "Error"
+        fi
+        sleep_duration=$(($RANDOM % 100))
+        sleep_duration=$((sleep_duration+5))
+        echo sleep for $sleep_duration seconds on `date`;
+        rtcwake -s $sleep_duration -m mem 
+        echo "resumed" $1 "time on" `date`
+        i=$((i+5))
+        sleep 5
+done
